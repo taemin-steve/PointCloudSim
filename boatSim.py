aabf04a5959c9a697d76bfd35711a0173657a7a5
taemin-steve <okchrisok@naver.com>
2023-02-21 18:28:46 +0900

    scene clear

--- boatSim.py
+++ boatSim.py
@@ -1,102 +1,56 @@
 from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from panda3d.core import Geom, GeomVertexData, GeomVertexFormat, GeomVertexWriter
-from panda3d.core import GeomTriangles, GeomNode
-from panda3d.core import Point3
-import math
-from panda3d.core import Camera
-from panda3d.core import LPoint3f
-from panda3d.core import NodePath, LPoint3
-from panda3d.core import PerspectiveLens, LVector3
+from panda3d.core import loadPrcFileData
+from panda3d.core import Shader
+from draw_sphere import draw_sphere
 import panda3d.core as p3d
 
+configVars = """
+win-size 1280 720
+show-frame-rate-meter 1
+"""
 
-class MyApp(ShowBase):
+loadPrcFileData("", configVars)
+
+class MyGame(ShowBase):
     def __init__(self):
-        ShowBase.__init__(self)
-
-        # 구체의 반지름과 분할 수를 정의합니다.
-        
-        def make_sphere(radius,pos,color):
-            subdivisions = 32
-            
-             # 구체의 정점 데이터를 생성합니다.
-            format = GeomVertexFormat.getV3()
-            vdata = GeomVertexData('sphere', format, Geom.UHStatic)
-            vertex = GeomVertexWriter(vdata, 'vertex')
-            for i in range(subdivisions):
-                for j in range(subdivisions):
-                    theta = (i / subdivisions) * 2 * math.pi
-                    phi = (j / subdivisions) * math.pi
-                    x = radius * math.sin(phi) * math.cos(theta)
-                    y = radius * math.sin(phi) * math.sin(theta)
-                    z = radius * math.cos(phi)
-                    vertex.addData3f(x, y, z)
-
-            # 구체의 삼각형 데이터를 생성합니다.
-            tris = GeomTriangles(Geom.UHStatic)
-            for i in range(subdivisions):
-                for j in range(subdivisions):
-                    a = i * subdivisions + j
-                    b = (i + 1) % subdivisions * subdivisions + j
-                    c = i * subdivisions + (j + 1) % subdivisions
-                    tris.addVertices(a, b, c)
-                    tris.closePrimitive()
-
-                    a = (i + 1) % subdivisions * subdivisions + j
-                    b = (i + 1) % subdivisions * subdivisions + (j + 1) % subdivisions
-                    c = i * subdivisions + (j + 1) % subdivisions
-                    tris.addVertices(a, b, c)
-                    tris.closePrimitive()
-
-            # 구체의 노드를 생성하고, 삼각형과 정점 데이터를 추가합니다.
-            geom = Geom(vdata)
-            geom.addPrimitive(tris)
-            node = GeomNode('sphere')
-            node.addGeom(geom)
-            
-            # 구체를 렌더링합니다.
-            sphere = self.render.attachNewNode(node)
-            sphere.set_color(color)
-            sphere.setPos(pos)
-            sphere.setTwoSided(True)
+        super().__init__()
         
         #구를 직접 생성
-        make_sphere(0.1,(0,0,0),(1,0,0,1))
-        make_sphere(0.1,(0,0,2),(1,1,0,1))
-        make_sphere(0.1,(0,0,4),(1,0,1,1))
-        make_sphere(0.1,(0,0,6),(0,0,1,1))
-        make_sphere(0.1,(0,0,8),(0,1,0,1))
+        draw_sphere(self,0.1,(0,0,0),(1,0,0,1))
+        # draw_sphere(self,0.1,(0,0,2),(1,1,0,1))
+        # draw_sphere(self,0.1,(0,0,4),(1,0,1,1))
+        # draw_sphere(self,0.1,(0,0,6),(0,0,1,1))
+        # draw_sphere(self,0.1,(0,0,8),(0,1,0,1))
+        
+        # 카메라 각각의 좌표계를 설정
+        # camera_front = p3d.NodePath("ChildNode")
+        # camera_front.reparent_to(self.render)
+        # camera_front.setPos(2,2,2)
+        # camera_front.set_hpr(0, 0, 45)
+        # self.boat.reparentTo(camera_front)
+        
+        ############################### 위쪽 부분은 point cloud 를 생성하기 위한 파트.#########################
+        
+        # 카메라 위치 조정 
+        self.cam.setPos(0, -40, 0)
         
         #보트 로드
         self.boat = self.loader.loadModel("avikus_boat.glb")
+        self.boat.setPos(0,0,1.05)
         self.boat.reparentTo(self.render)
         
-        # self.scene = self.loader.loadModel("models/environment")
-        # self.scene.reparentTo(self.render)
         
-        # self.scene.setScale(0.25, 0.25, 0.25)
-        # self.scene.setPos(-8, 42, 0)
-        
-        # self.camera.setPos(0, 10, 0)
-        # self.camera.lookAt(0, 0, 0)
-        
-        # 카메라 각각의 좌표계를 설정
-        camera_front = p3d.NodePath("ChildNode")
-        camera_front.reparent_to(self.render)
-        camera_front.setPos(2,2,2)
-        camera_front.set_hpr(0, 0, 45)
-        self.boat.reparentTo(camera_front)
-        
-        # 카메라 생성
-        # base.disableMouse() # 마우스로 컨트롤을 할 수 있게 설정하는 경우 camera가 default로 이동해버림.
-        self.camera.setPos(20,20,20)
-        self.camera.lookAt(0,0,0)
-        
-        shader = Shader.load(Shader.SL_GLSL,
-                     vertex="myshader.vert",
-                     fragment="myshader.frag")
-        model.setShader(shader)
+        #쉐이더 설정 
+        my_shader = Shader.load(Shader.SL_GLSL,
+                                vertex="shaders/texturing-vert.glsl",
+                                fragment="shaders/texturing-frag.glsl")
 
-app = MyApp()
-app.run()
+        self.plane = self.loader.loadModel("models/cam_front")
+        self.plane.reparentTo(self.render)
+        self.plane.setShader(my_shader)
+        self.plane.setPos(-1,0,0)
+        self.plane.set_hpr(90, -90, 0)
+
+
+game = MyGame()
+game.run()
